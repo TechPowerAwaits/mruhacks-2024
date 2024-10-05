1797ecdfa1385d7be6fa5d623c32d4c00403df14
Trey London <tlond369@mtroyal.ca>
2024-10-05 14:35:13 -0600

    Titlescreen pretty done

---
+++ Scenes/text_edit.gd
@@ -0,0 +1,13 @@
+extends TextEdit
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	add_theme_font_size_override("font_size", 40)
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

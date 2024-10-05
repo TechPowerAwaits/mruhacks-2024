f72ae147efa5e16ff38352c8bc93a5b225846a31
Richard Johnston <techpowerawaits@outlook.com>
2024-10-05 14:42:49 -0600

    Attempt to add new level sprites
    The exported variables do not appear in the inspector.

--- Scenes/Creatures/player.gd
+++ Scenes/Creatures/player.gd
@@ -1,21 +1,46 @@
 extends CharacterBody2D
 
+const L1_IMAGE: Texture = preload("res://Assets/player_lvl_1.png")
+const FALLBACK_IMAGE: Texture = preload("res://Assets/Player.png")
+
+
+enum PlayerType {
+	LEVEL1,
+	LEVEL2,
+	LEVEL3
+}
 
 @export var cell_size := 16
+@export var player_state := PlayerType.LEVEL1:
+	get = get_player_state,
+	set = set_player_state
 
 
 func _physics_process(_delta: float) -> void:
-	var x_modifier := 0
-	var y_modifier := 0
+	pass
+	#var x_modifier := 0
+	#var y_modifier := 0
 	
-	if Input.is_action_pressed("Move down"):
-		y_modifier += cell_size
-	if Input.is_action_pressed("Move up"):
-		y_modifier += -cell_size
-	if Input.is_action_pressed("Move left"):
-		x_modifier += -cell_size
-	if Input.is_action_pressed("Move right"):
-		x_modifier += cell_size
+	#if Input.is_action_pressed("Move down"):
+	#	y_modifier += cell_size
+	#if Input.is_action_pressed("Move up"):
+	#	y_modifier += -cell_size
+	#if Input.is_action_pressed("Move left"):
+	#	x_modifier += -cell_size
+	#if Input.is_action_pressed("Move right"):
+	#	x_modifier += cell_size
 	
-	position.x += x_modifier
-	position.y += y_modifier
+	#position.x += x_modifier
+	#position.y += y_modifier
+
+
+func set_player_state(value: PlayerType) -> void:
+	match value:
+		LEVEL1:
+			$Sprite2D.texture = L1_IMAGE
+		_:
+			$Sprite2D.texture = FALLBACK_IMAGE
+
+
+func get_player_state() -> PlayerType:
+	return 0

3e8f4023b080d76f2cc2615a51e9a7dbbc1a7c4a
Richard Johnston <techpowerawaits@outlook.com>
2024-10-06 05:07:32 -0600

    Add API to DialogueBox
    Allows it to be inserted into other scenes.

--- Scenes/DialogueBox.gd
+++ Scenes/DialogueBox.gd
@@ -1,27 +1,43 @@
-extends Node
+extends Panel
 
 const DIALOGUE_LINES := 5
 const DIALOGUE_FILE := "res://Dialogue.txt"
 
 var text_file: FileAccess = null
 var counter := 1
+var num_lines := 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	text_file = FileAccess.open(DIALOGUE_FILE, FileAccess.READ)
-	counter = 1
-	$DialogueText.text = text_file.get_line()
-	
+	hide()
 
-func _on_talk_button_pressed() -> void:
-	if counter < DIALOGUE_LINES:
-		counter += 1
-		$DialogueText.text = text_file.get_line()
-	else:
-		$DialogueText.text = "Please Press Leave."
-	
-func _on_leave_button_pressed() -> void:
+
+# Can get away with assuming that reading never fails, since it is stored within the project.
+func load_text(path: String, number_lines: int) -> void:
+	text_file = FileAccess.open(path, FileAccess.READ)
+	show()
+	$DialogueText.txt = text_file.get_line()
+	counter = 1
+	num_lines = number_lines
+
+
+func _close_text() -> void:
+	hide()
+	counter = 1
+	num_lines = 0
 	if text_file != null:
 		text_file.close()
 		text_file = null
-	get_tree().quit()
+
+
+func _on_talk_button_pressed() -> void:
+	if counter < num_lines:
+		counter += 1
+		$DialogueText.text = text_file.get_line()
+	else:
+		_close_text()
+
+
+func _on_leave_button_pressed() -> void:
+	_close_text()

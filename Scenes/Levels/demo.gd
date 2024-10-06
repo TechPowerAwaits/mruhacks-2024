03d138fde6aa7fa3f4c1bd3d4cee37f3d478624c
Richard Johnston <techpowerawaits@outlook.com>
2024-10-05 23:04:59 -0600

    Utilize CollectibleManager in demo level

--- Scenes/Levels/demo.gd
+++ Scenes/Levels/demo.gd
@@ -10,3 +10,11 @@ func _ready() -> void:
 
 func _on_collectible_collected(type: Variant) -> void:
 	print("Congrat on collecting a collectable of type %s." % type)
+
+
+func _on_collectible_manager_collected(type: Variant) -> void:
+	print("The CollectibleManager notes %s has been collected." % type)
+
+
+func _on_collectible_manager_all_collected() -> void:
+	print("All collectibles have been collected.")

8c3d1557026d65d3a40104ea78568335f3ad3b92
Richard Johnston <techpowerawaits@outlook.com>
2024-10-06 02:33:58 -0600

    Use one file for first part of story

--- Scenes/DialogueBox.gd
+++ Scenes/DialogueBox.gd
@@ -1,20 +1,27 @@
 extends Node
 
-@onready var counter = 1
-@onready var format_string = "res://Dialogue%s.txt"
-@onready var D1 = format_string % counter
+const DIALOGUE_LINES := 5
+const DIALOGUE_FILE := "res://Dialogue.txt"
+
+var text_file: FileAccess = null
+var counter := 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$DialogueText.text = FileAccess.get_file_as_string(D1)
+	text_file = FileAccess.open(DIALOGUE_FILE, FileAccess.READ)
+	counter = 1
+	$DialogueText.text = text_file.get_line()
+	
 
 func _on_talk_button_pressed() -> void:
-	if counter < 6:
+	if counter < DIALOGUE_LINES:
 		counter += 1
-		D1 = format_string % counter
-		$DialogueText.text = FileAccess.get_file_as_string(D1)
+		$DialogueText.text = text_file.get_line()
 	else:
 		$DialogueText.text = "Please Press Leave."
 	
 func _on_leave_button_pressed() -> void:
+	if text_file != null:
+		text_file.close()
+		text_file = null
 	get_tree().quit()
